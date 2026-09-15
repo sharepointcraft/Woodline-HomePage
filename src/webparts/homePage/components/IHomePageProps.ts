d054fc32e87fc3c3a53fdd34af46227e35c16500
sharepointcraft <sharepointcraft@gmail.com>
2026-09-15 14:07:41 +0530

    Initial Setup of Woodline-Homepage

---
+++ src/webparts/homePage/components/IHomePageProps.ts
@@ -0,0 +1,6 @@
+export interface IHomePageProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ src/webparts/homePage/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHomePageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HomePageWebPartStrings' {
+  const strings: IHomePageWebPartStrings;
+  export = strings;
+}
